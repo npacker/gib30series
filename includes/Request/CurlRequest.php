885b0e85778237bdd9774ed32c6150b450c050e2
Nigel Packer <packern@wwu.edu>
2020-12-09 04:19:20 +0000

    Make CurlRequest reusable

--- includes/Request/CurlRequest.php
+++ includes/Request/CurlRequest.php
@@ -2,30 +2,33 @@
 
 class CurlRequest {
 
-  private $cainfo;
+  private $handle;
 
   public function __construct(string $cainfo) {
-    $this->cainfo = $cainfo;
+    $this->handle = curl_init();
+
+    curl_setopt($this->handle, CURLOPT_FRESH_CONNECT, TRUE);
+    curl_setopt($this->handle, CURLOPT_FORBID_REUSE, TRUE);
+    curl_setopt($this->handle, CURLOPT_ENCODING, '');
+    curl_setopt($this->handle, CURLOPT_HEADER, FALSE);
+    curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($this->handle, CURLOPT_SSL_VERIFYPEER, TRUE);
+    curl_setopt($this->handle, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($this->handle, CURLOPT_CAINFO, $cainfo);
+  }
+
+  public function __destruct() {
+    curl_close($this->handle);
   }
 
   public function send(string $url): string {
-    $handle = curl_init($url);
+    curl_setopt($this->handle, CURLOPT_URL, $url);
 
-    curl_setopt($handle, CURLOPT_FRESH_CONNECT, TRUE);
-    curl_setopt($handle, CURLOPT_FORBID_REUSE, TRUE);
-    curl_setopt($handle, CURLOPT_HEADER, FALSE);
-    curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, TRUE);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($handle, CURLOPT_CAINFO, $this->cainfo);
-
-    $buffer = curl_exec($handle);
+    $buffer = curl_exec($this->handle);
 
     if ($buffer === false) {
-      throw new Exception(curl_error($handle));
+      throw new Exception(curl_error($this->handle));
     }
-
-    curl_close($handle);
 
     return $buffer;
   }
