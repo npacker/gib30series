b49465fea2132f3a2e8fe444e19155507fe35d59
Nigel Packer <packern@wwu.edu>
2022-01-15 00:20:43 +0000

    Update default settings.php

--- includes/default.settings.php
+++ includes/default.settings.php
@@ -2,8 +2,63 @@
 
 /**
  * $settings = [
- *   'url' => 'https://www.newegg.com/p/pl?N=100007709%20601357250',
- *   'discord_webhook' => '',
+ *   'timeout' => 1000,
+ *   'status' => [
+ *     'IN_STOCK' => Status::IN_STOCK,
+ *     'SOLD_OUT' => Status::SOLD_OUT,
+ *     'AUTO_NOTIFY' => Status::AUTO_NOTIFY,
+ *   ],
+ *   'discord_webhook' => 'https://discordapp.com/api/webhooks',
  *   'cainfo' => '/etc/ssl/certs/USERTrust_RSA_Certification_Authority.pem',
+ *   'endpoints' => [
+ *     [
+ *       'type' => NeweggItems::class,
+ *        'url' => 'https://www.newegg.com/p/pl?N=100007709%20601357250',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/evga-geforce-rtx-3070-08g-p5-3753-kr/p/N82E16814487529',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' =>  'https://www.newegg.com/evga-geforce-rtx-3070-08g-p5-3765-kr/p/N82E16814487531',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/evga-geforce-rtx-3070-08g-p5-3755-kr/p/N82E16814487530',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/evga-geforce-rtx-3070-08g-p5-3767-kr/p/N82E16814487532',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/gigabyte-geforce-rtx-3070-gv-n3070aorus-m-8gd/p/N82E16814932359',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/msi-geforce-rtx-3070-rtx-3070-gaming-x-trio/p/N82E16814137603',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url'  => 'https://www.newegg.com/msi-geforce-rtx-3070-rtx-3070-ventus-3x-oc/p/N82E16814137601',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url'  => 'https://www.newegg.com/asus-geforce-rtx-3070-tuf-rtx3070-o8g-gaming/p/N82E16814126461',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/gigabyte-geforce-rtx-3070-gv-n3070gaming-oc-8gd/p/N82E16814932342',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/gigabyte-geforce-rtx-3070-gv-n3070vision-oc-8gd/p/N82E16814932360',
+ *     ],
+ *     [
+ *       'type' => NeweggProduct::class,
+ *       'url' => 'https://www.newegg.com/asus-geforce-rtx-3070-rog-strix-rtx3070-o8g-gaming/p/N82E16814126458',
+ *     ],
+ *   ],
  * ];
  */
